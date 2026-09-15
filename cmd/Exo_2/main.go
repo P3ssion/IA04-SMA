1f2099377ecc7789015bed0ec460faf567b444f1
alicia <alicia.randrianarimanana@gmail.com>
2026-09-15 18:05:36 +0200

    Exercice 2 termine

--- cmd/Exo_2/main.go
+++ cmd/Exo_2/main.go
@@ -7,6 +7,45 @@ import (
 
 func main() {
 	//arr1 := [...]int{1, 2, 3, 4}
+
+	fmt.Println("DEBUT DES TESTS")
+	slt := make([]int, 5)
+	Fills(slt)
+	moyenneAvant := Moyenne(slt)
+	centreAvant := ValeursCentrales(slt)
+
+	fmt.Println("Slice initial: ", slt)
+	fmt.Println("Moyenne avant : ", moyenneAvant)
+	fmt.Println("Valeurs centrales avant : ", centreAvant)
+
+	fmt.Println("---Application de Plus1---")
+	Plus1(slt)
+
+	moyenneApres := Moyenne(slt)
+	centreApres := ValeursCentrales(slt)
+
+	fmt.Println("Slice final : ", slt)
+	fmt.Println("Moyenne apres: ", moyenneApres)
+	fmt.Println("Centres apres : ", centreApres)
+
+	fmt.Println("Fin des tests")
+
+	maListe := make([]int, 5)
+
+	monSlice := make([]int, 5)
+	Fills(monSlice)
+	fmt.Println("Résultat du slice que j'ai Fills :", monSlice)
+
+	fmt.Println("Valeurs centrales :", ValeursCentrales(monSlice))
+
+	var monTableau [6]int
+	Fills(monTableau[:])
+	fmt.Println("Résultat du tableau :", monTableau)
+	fmt.Println("Valeurs centrales tableau cases paires :", ValeursCentrales(monTableau[:]))
+
+	RemplirAvecDesZero(maListe)
+	fmt.Println(maListe)
+
 	fmt.Println("Q0 Fonction pair")
 	pair()
 
@@ -17,6 +56,10 @@ func main() {
 	fmt.Println(sl)
 	fmt.Println("Q2 Moyenne")
 	fmt.Println(Moyenne(sl))
+
+	// On calcule la moyenne de monSlice
+	maMoyenne := Moyenne(monSlice)
+	fmt.Println("La moyenne est :", maMoyenne)
 
 	fmt.Println("Q4 Plus1")
 	fmt.Println(sl)
@@ -36,6 +79,13 @@ func Fills(sl []int) {
 	for i := range sl {
 		sl[i] = rand.Intn(100)
 		//fmt.Println(sl[i])
+		//Pas besoin car on peut juste print le slice directement
+	}
+}
+
+func RemplirAvecDesZero(sl []int) {
+	for index := range sl {
+		sl[index] = 0
 	}
 }
 
@@ -54,5 +104,17 @@ func Moyenne(nombres []int) float64 {
 func Plus1(sl []int) {
 	for i := range sl {
 		sl[i] = sl[i] + 1
+	}
+}
+
+// Q5
+func ValeursCentrales(sl []int) []int {
+	taille := len(sl)
+	milieu := taille / 2
+
+	if taille%2 == 0 {
+		return []int{sl[milieu-1], sl[milieu]}
+	} else {
+		return []int{sl[milieu]}
 	}
 }
